43447bb582cdf6269ecd301a6e41b7f85aeeec53
jake <jake.gergen714@gmail.com>
2024-10-23 09:22:20 -0400

    refactored sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   siteUrl: 'https://www.rahimicustomconstruction.com', // Replace with your actual domain
   generateRobotsTxt: true, // Automatically generate a robots.txt file
-  exclude: ['/invoices', '/login', '/gallery'], // Exclude admin pages from the sitemap
+  exclude: ['/invoices', '/login'], // Exclude admin pages from the sitemap
   robotsTxtOptions: {
     policies: [
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/invoices', '/login', '/gallery'],
+        disallow: ['/invoices', '/login'],
       }, // Disallow crawling admin pages
     ],
   },
