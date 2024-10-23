9c40af54fc4e467817149611d82a0e4cb5f99b69
jake <jake.gergen714@gmail.com>
2024-10-23 09:25:23 -0400

    added loading animation to client side gallery

--- src/app/gallery/GallerySlider.js
+++ src/app/gallery/GallerySlider.js
@@ -2,12 +2,20 @@
 
 import dynamic from 'next/dynamic';
 import Image from 'next/image';
+import { useState, useEffect } from 'react';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const Slider = dynamic(() => import('react-slick'), { ssr: false });
 
 export default function GallerySlider({ images }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Set loading to false once the Slider component is mounted
+    setLoading(false);
+  }, []);
+
   const settings = {
     dots: true,
     infinite: true,
@@ -29,26 +37,52 @@ export default function GallerySlider({ images }) {
 
   return (
     <div className='items-center'>
-      <Slider {...settings}>
-        {images.map((curImage, index) => (
-          <div
-            key={index}
-            className='relative flex justify-center items-center h-[80vh] sm:h-screen'
-          >
-            <div className='relative w-full h-full flex justify-center items-center'>
-              <Image
-                src={curImage.src}
-                layout='fill'
-                objectFit='contain'
-                alt={curImage.alt}
-              />
-              <div className='absolute bottom-0 w-full bg-black bg-opacity-70 text-white text-center py-3'>
-                <p className='text-lg'>{curImage.alt}</p>
+      {loading && (
+        <div className='flex justify-center items-center h-screen'>
+          <div className='loader'></div>
+        </div>
+      )}
+      {!loading && (
+        <Slider {...settings}>
+          {images.map((curImage, index) => (
+            <div
+              key={index}
+              className='relative flex justify-center items-center h-[80vh] sm:h-screen'
+            >
+              <div className='relative w-full h-full flex justify-center items-center'>
+                <Image
+                  src={curImage.src}
+                  layout='fill'
+                  objectFit='contain'
+                  alt={curImage.alt}
+                />
+                <div className='absolute bottom-0 w-full bg-black bg-opacity-70 text-white text-center py-3'>
+                  <p className='text-lg'>{curImage.alt}</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </Slider>
+          ))}
+        </Slider>
+      )}
+      <style jsx>{`
+        .loader {
+          border: 8px solid #f3f3f3; /* Light gray */
+          border-top: 8px solid #3498db; /* Blue */
+          border-radius: 50%;
+          width: 60px;
+          height: 60px;
+          animation: spin 1.5s linear infinite;
+        }
+
+        @keyframes spin {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+      `}</style>
     </div>
   );
 }
